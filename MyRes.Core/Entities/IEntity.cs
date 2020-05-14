fe7ec5bf92de74e5cd4449e8af338b82ae7951f8
enis ertem <enis394@hotmail.com>
2020-05-14 12:38:17 +0300

    Coding Entities

---
+++ MyRes.Core/Entities/IEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyRes.Core.Entities
+{
+    public interface IEntity
+    {
+    }
+}
---
+++ MyRes.Entities/Concrete/UserResume.cs
@@ -0,0 +1,14 @@
+﻿using MyRes.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyRes.Entities.Concrete
+{
+    public class UserResume:IEntity
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int ResumeCategoryId { get; set; }
+    }
+}
